f9336f7b1742286032dbfb916dae84492b468193
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-02 11:23:27 -0800

    All tests passing except final add and remove from queue test

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -3,5 +3,24 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Views = window.MyTunes.Views || {};
 
 MyTunes.Views.SongQueueEntryView = Backbone.View.extend({
+  tagName: 'tr',
 
+  template: _.template('<td>(<%= this.artist %>)</td><td><%= this.title %></td>'),
+
+  events: {
+    'click': function() {
+      this.model.play();
+    }
+    // 'remove': function(){
+    //   console.log("We're being removed");
+    // },
+    // 'add': function() {
+    //   console.log("We're being added!");
+    // }
+  },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
+
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -5,10 +5,22 @@ window.MyTunes.Views = window.MyTunes.Views || {};
 MyTunes.Views.SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+     this.collection.on('remove', function() {
+       console.log("We're being removed");
+     },this);
+     this.collection.on('add', function() {
+       console.log("We're being added");
+     },this);
   },
 
   render: function() {
-    return this.$el;
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+    this.$el.html('<th>queue</th>').append(
+      this.collection.map(function(song){
+        return new MyTunes.Views.SongQueueEntryView({model: song}).render();
+      })
+    );
   }
-
 });
