5291c3a565926ace0148a6f6a6cee560f17e1511
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-02 20:05:04 -0800

    Cleaning up

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -9,7 +9,10 @@ MyTunes.Views.PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
-
+    var that = this.model;
+    this.$el.on('ended',function(){
+      that.ended();
+    });
   },
 
   setSong: function(song){
@@ -18,11 +21,6 @@ MyTunes.Views.PlayerView = Backbone.View.extend({
   },
 
   render: function(){
-    var that = this.model;
-    this.$el.on('ended',function(){
-      that.ended();
-    });
-
     return this.$el.attr('src', this.model.get('url'));
   }
 
