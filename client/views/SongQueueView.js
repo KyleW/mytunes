11e05f68328505a302ed475dcd813c1b1e99d460
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-02 16:12:37 -0800

    Some refactoring

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -3,7 +3,7 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Views = window.MyTunes.Views || {};
 
 MyTunes.Views.SongQueueView = Backbone.View.extend({
-
+  tagName: "table",
   initialize: function() {
     this.render();
 
@@ -20,7 +20,7 @@ MyTunes.Views.SongQueueView = Backbone.View.extend({
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
     // see http://api.jquery.com/detach/
     this.$el.children().detach();
-    this.$el.html('<th>Queue</th>').append(
+    this.$el.html('<th>Queue</th>').addClass('songQueue').append(
       this.collection.map(function(song){
         return new MyTunes.Views.SongQueueEntryView({model: song}).render();
       })
